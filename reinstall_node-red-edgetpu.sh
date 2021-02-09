8429f16aa5afe00f2a2f7254b9e4f932a85b20dd
Vic Hsu <Vic_Hsu@asus.com>
2021-02-09 17:12:53 +0800

    Modify the chart line of the performance node
    Change-Id: Id81ff5b76c3b201694a5ad00725248b8f40a1342

--- reinstall_node-red-edgetpu.sh
+++ reinstall_node-red-edgetpu.sh
@@ -1,4 +1,6 @@
 cd ~/.node-red
 npm uninstall node-red-contrib-edgetpu-inference
+sudo apt-get install python3-pycoral
+sudo pip3 install "https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp36-cp36m-linux_x86_64.whl"
 cd -
 npm install --prefix=~/.node-red ../node-red-contrib-edgetpu-inference
